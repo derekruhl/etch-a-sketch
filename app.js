ffa6564304f932d983457afa4ed59072470e4a34
Derek Ruhl <128940334+derekruhl@users.noreply.github.com>
2024-02-05 15:52:07 -0700

    tried to figure out why the border won't appear on a blank div

--- app.js
+++ app.js
@@ -2,5 +2,6 @@ const container = document.querySelector(".container");
 const grid = document.createElement('div');
 grid.classList.add('grid');
 container.appendChild(grid);
+grid.textContent = 'A';
 
 //need to make several rows/columns with flexbox. how?
