4df88e0ab5261f2eab4c76451e8b14f80ff8f33b
Derek Ruhl <128940334+derekruhl@users.noreply.github.com>
2024-02-05 15:30:34 -0700

    started connecting to the DOM

---
+++ app.js
@@ -0,0 +1,6 @@
+const container = document.querySelector(".container");
+const grid = document.createElement('div');
+grid.classList.add('grid');
+container.appendChild(grid);
+
+//need to make several rows/columns with flexbox. how?
