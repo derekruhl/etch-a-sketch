2d29055925f1f9c70250730840be03e13b9b898b
Derek Ruhl <128940334+derekruhl@users.noreply.github.com>
2024-02-09 15:37:41 -0700

    started adding rows and columns in app.js

--- app.js
+++ app.js
@@ -5,3 +5,13 @@ container.appendChild(grid);
 grid.textContent = 'A';
 
 //need to make several rows/columns with flexbox. how?
+
+let rows = 16;
+let columns = 16;
+
+const userRows = document.createAttribute('div');
+const userColumns = document.createAttribute('div');
+
+//append child usercolumns to grid
+//keep rows as children of columns
+//how to append a specific numbr of children to grid/column/row???
