ba242d85ebb5b0f3819b136ea748caec24bcefe5
Derek Ruhl <128940334+derekruhl@users.noreply.github.com>
2024-02-09 15:54:15 -0700

    wrote function to get grid size and loop to create grid

--- app.js
+++ app.js
@@ -9,9 +9,24 @@ grid.textContent = 'A';
 let rows = 16;
 let columns = 16;
 
-const userRows = document.createAttribute('div');
-const userColumns = document.createAttribute('div');
+
 
 //append child usercolumns to grid
 //keep rows as children of columns
 //how to append a specific numbr of children to grid/column/row???
+//loop over a number of times input by the user??
+
+let getGridSize = function () {
+   return Number.(window.prompt('How many rows and columns would you like?, 16'));
+}
+
+getGridSize();
+
+for (let i = 0; i < userRows; i++) {
+    const userRows = document.createAttribute('div');
+    const userColumns = document.createAttribute('div');
+    userRows.classList.add('rows');
+    userColumns.classList.add('columns');
+    grid.appendChild(userColumns);
+    userColumns.appendChild(userRows);
+}
