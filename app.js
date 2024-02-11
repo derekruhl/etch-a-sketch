5355d5dab17cc8a63d1834866f092f62a2d826c3
Derek Ruhl <128940334+derekruhl@users.noreply.github.com>
2024-02-10 20:20:42 -0700

    added style rules.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const container = document.querySelector(".container");
 
 for (let i = 1; i < 257; i++) {
-    const userRows = document.createElement('div');
+    const grid = document.createElement('div');
     userRows.style.cssText = "border: 1px solid black; height: 25px; width: 25px";
+    container.appendChild(grid);
 }
