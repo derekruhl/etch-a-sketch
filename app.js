51ffba108492151d91c27b798f5491381c28737b
Derek Ruhl <128940334+derekruhl@users.noreply.github.com>
2024-02-10 20:07:40 -0700

    wiped it and started from scratch

--- app.js
+++ app.js
@@ -1,32 +1,6 @@
 const container = document.querySelector(".container");
-const grid = document.createElement('div');
-grid.classList.add('grid');
-container.appendChild(grid);
-grid.textContent = 'A';
 
-//need to make several rows/columns with flexbox. how?
-
-let rows = 16;
-let columns = 16;
-
-
-
-//append child usercolumns to grid
-//keep rows as children of columns
-//how to append a specific numbr of children to grid/column/row???
-//loop over a number of times input by the user??
-
-let getGridSize = function () {
-   return Number.(window.prompt('How many rows and columns would you like?, 16'));
-}
-
-getGridSize();
-
-for (let i = 0; i < userRows; i++) {
-    const userRows = document.createAttribute('div');
-    const userColumns = document.createAttribute('div');
-    userRows.classList.add('rows');
-    userColumns.classList.add('columns');
-    grid.appendChild(userColumns);
-    userColumns.appendChild(userRows);
+for (let i = 1; i < 257; i++) {
+    const userRows = document.createElement('div');
+    userRows.style.cssText = "border: 1px solid black; height: 25px; width: 25px";
 }
