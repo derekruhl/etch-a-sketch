8e93f42af9728a4187b9eabd4e11e2f6a88ce8fd
Derek Ruhl <128940334+derekruhl@users.noreply.github.com>
2024-02-12 16:33:17 -0700

    add flex style to the individual divs

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const grid = document.querySelector('.container');
+const cdiv = document.querySelector('.container');
 
 for (let i = 1; i < 257; i++) {
-    const square = document.createElement('div');
-    grid.classList = 'grid';
-    grid.appendChild(square);
-    square.style.cssText = 'border: 1px solid black; height: 25px; width: 25px';
+    const div = document.createElement('div');
+   
+    cdiv.appendChild(div);
+    
     
 }
