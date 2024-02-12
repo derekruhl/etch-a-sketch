f9dbc2faee26456e7e981396666b733a58ac81f6
Derek Ruhl <128940334+derekruhl@users.noreply.github.com>
2024-02-12 15:57:10 -0700

    wiped everything and started fresh

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-const container = document.querySelector(".container");
+const grid = document.querySelector('.container');
 
 for (let i = 1; i < 257; i++) {
-    const grid = document.createElement('div');
-    userRows.style.cssText = "border: 1px solid black; height: 25px; width: 25px";
-    container.appendChild(grid);
+    const square = document.createElement('div');
+    grid.classList = 'grid';
+    grid.appendChild(square);
+    square.style.cssText = 'border: 1px solid black; height: 25px; width: 25px';
+    
 }
