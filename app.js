3ed94dd096ad81f0c6e1f7675f0bdf179dff2204
Derek Ruhl <128940334+derekruhl@users.noreply.github.com>
2024-02-17 14:31:14 -0700

    fixed event listener to mouseover

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const cdiv = document.querySelector('.container');
 
 for (let i = 1; i < 257; i++) {
     const div = document.createElement('div');
-   
-    cdiv.appendChild(div);
-    
-    
+    cdiv.appendChild(div); 
 }
+
+document.addEventListener ('mouseover', () => {
+    document.querySelector(div);
+    document.style.backgroundColor = 'red';
+})
